a1b772929a513c91f0c97c1af4d660101cb5d1a6
fnsl1026 <fnsl1026@naver.com>
2024-11-07 18:59:05 +0900

    feat: RegisteredPhoneNumber 엔티티 생성 (#2)

--- src/main/java/com/team/buddyya/certification/domain/RegisteredPhoneNumber.java
+++ src/main/java/com/team/buddyya/certification/domain/RegisteredPhoneNumber.java
@@ -1,4 +1,33 @@
 package com.team.buddyya.certification.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.*;
+
+@Getter
+@Table(name = "registered_phone_number")
+@NoArgsConstructor(access = PROTECTED)
+@Entity
 public class RegisteredPhoneNumber {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(length = 11, nullable = false,unique = true)
+    private String phoneNumber;
+
+    @Column(length = 6, nullable = false,unique = true)
+    private String authenticationCode;
+
+    public RegisteredPhone(String phoneNumber, String authenticationCode) {
+        this.phoneNumber = phoneNumber;
+        this.authenticationCode = authenticationCode;
+    }
+
+    public void setAuthenticationCode(String authenticationCode) {
+        this.authenticationCode = authenticationCode;
+    }
 }
