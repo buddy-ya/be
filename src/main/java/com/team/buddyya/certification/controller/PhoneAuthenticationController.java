4bcb15e1aaa19d9a17d100f234f9b0fcb66a75b5
fnsl1026 <fnsl1026@naver.com>
2024-11-07 19:33:30 +0900

    feat: 인증 코드 보내는 controller 구현 (#2)

---
+++ src/main/java/com/team/buddyya/certification/controller/PhoneAuthenticationController.java
@@ -0,0 +1,31 @@
+package com.team.buddyya.certification.controller;
+
+import com.team.buddyya.certification.dto.request.SendCodeRequest;
+import com.team.buddyya.certification.dto.request.VerifyCodeRequest;
+import com.team.buddyya.certification.dto.response.SendCodeResponse;
+import com.team.buddyya.certification.dto.response.VerifyCodeResponse;
+import com.team.buddyya.certification.service.PhoneAuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/phone-auth")
+@RestController
+public class PhoneAuthenticationController {
+
+    private final PhoneAuthenticationService phoneAuthenticationService;
+
+        @PostMapping("/send-code")
+        public ResponseEntity<SendCodeResponse> sendOne(@RequestBody SendCodeRequest sendCodeRequest) {
+
+            String generatedCode = phoneAuthenticationService.sendSms(sendCodeRequest.phoneNumber());
+            SendCodeResponse response = phoneAuthenticationService.saveCode(sendCodeRequest.phoneNumber(), generatedCode);
+
+            return ResponseEntity.ok(response);
+        }
+
+}
