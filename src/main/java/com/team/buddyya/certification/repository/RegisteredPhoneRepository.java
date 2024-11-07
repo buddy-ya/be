033de373bc8461c864fcab188548494ffa104725
fnsl1026 <fnsl1026@naver.com>
2024-11-07 19:36:47 +0900

    feat: RegisteredPhoneRepository에 phoneNumber로 RegisteredPhone을 찾는 기능 추가 (#2)

---
+++ src/main/java/com/team/buddyya/certification/repository/RegisteredPhoneRepository.java
@@ -0,0 +1,12 @@
+package com.team.buddyya.certification.repository;
+
+import com.team.buddyya.certification.domain.RegisteredPhone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RegisteredPhoneRepository extends JpaRepository<RegisteredPhone, Long> {
+    Optional<RegisteredPhone> findByPhoneNumber(String phoneNumber);
+}
