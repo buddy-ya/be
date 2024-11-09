ca7aa7097878b87876017188edd0b9304236f945
fnsl1026 <fnsl1026@naver.com>
2024-11-10 00:22:55 +0900

    refactor: 휴대폰 인증과 관련된 errorCode를 common에서 certification으로 이동 및 클래스 이름 변경 (#2)

--- src/main/java/com/team/buddyya/certification/exception/PhoneAuthenticationErrorCode.java
+++ src/main/java/com/team/buddyya/certification/exception/PhoneAuthenticationErrorCode.java
@@ -1,13 +1,14 @@
-package com.team.buddyya.common.exception;
+package com.team.buddyya.certification.exception;
 
-public enum ErrorCode {
+public enum PhoneAuthenticationErrorCode {
+
     SMS_SEND_FAILED("E2001", "SMS 전송 실패"),
     CODE_MISMATCH("E2002", "Authentication code가 일치하지 않습니다");
 
     private final String code;
     private final String message;
 
-    ErrorCode(String code, String message) {
+    PhoneAuthenticationErrorCode(String code, String message) {
         this.code = code;
         this.message = message;
     }
