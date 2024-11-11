e488e5006253e6ed114a4d308b2348456076e4a8
fnsl1026 <fnsl1026@naver.com>
2024-11-11 18:48:57 +0900

    feat: 학생의 관심사들을 저장하는 서비스 구현 (#3)

---
+++ src/main/java/com/team/buddyya/student/service/StudentInterestService.java
@@ -0,0 +1,31 @@
+package com.team.buddyya.student.service;
+
+import com.team.buddyya.student.domain.Interest;
+import com.team.buddyya.student.domain.Student;
+import com.team.buddyya.student.domain.StudentInterest;
+import com.team.buddyya.student.dto.request.OnBoardingRequest;
+import com.team.buddyya.student.exception.OnBoardingException;
+import com.team.buddyya.student.exception.OnBoardingExceptionType;
+import com.team.buddyya.student.repository.InterestRepository;
+import com.team.buddyya.student.repository.StudentInterestRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StudentInterestService {
+
+    private final StudentInterestRepository studentInterestRepository;
+    private final InterestRepository interestRepository;
+
+    public void createStudentInterests(OnBoardingRequest request, Student student) {
+        request.interests().forEach(interestName -> {
+            Interest interest = interestRepository.findByInterestName(interestName)
+                    .orElseThrow(() -> new OnBoardingException(OnBoardingExceptionType.INTEREST_NOT_FOUND));
+            studentInterestRepository.save(StudentInterest.builder()
+                    .student(student)
+                    .interest(interest)
+                    .build());
+        });
+    }
+}
