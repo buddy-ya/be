422ab8842b3957949d42e1596153aac99264491d
fnsl1026 <fnsl1026@naver.com>
2024-11-11 18:40:57 +0900

    feat: Avatar 엔티티에 온보딩에 필요한 필드 추가 (#3)

--- src/main/java/com/team/buddyya/student/domain/Avatar.java
+++ src/main/java/com/team/buddyya/student/domain/Avatar.java
@@ -1,28 +1,25 @@
 package com.team.buddyya.student.domain;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
+import static jakarta.persistence.FetchType.LAZY;
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
-@Getter
-@NoArgsConstructor(access = PROTECTED)
 @Entity
 @Table(name = "avatar")
+@Getter
+@NoArgsConstructor(access = PROTECTED)
 public class Avatar {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private Long version;
-
-    @Column(length = 11, nullable = false)
+    @Column(length = 15, nullable = false)
     private String phoneNumber;
 
     @Column(nullable = false)
@@ -34,7 +31,16 @@ public class Avatar {
     @Column(nullable = false)
     private Boolean loggedOut;
 
-    private LocalDateTime deactivatedDate;
+    @OneToOne(fetch = LAZY)
+    @JoinColumn(name = "student_id", nullable = false)
+    private Student student;
 
-    private LocalDateTime updatedDate;
+    @Builder
+    public Avatar(String phoneNumber, boolean notificationEnabled,Student student) {
+        this.phoneNumber = phoneNumber;
+        this.notificationEnabled = notificationEnabled;
+        this.active = true;
+        this.loggedOut = false;
+        this.student = student;
+    }
 }
