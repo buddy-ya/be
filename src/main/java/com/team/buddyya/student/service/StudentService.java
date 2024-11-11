f7a630081f5fa2e9497a5b959896899614907e50
fnsl1026 <fnsl1026@naver.com>
2024-11-11 18:49:28 +0900

    feat: 학생을 저장하는 서비스 구현 (#3)

---
+++ src/main/java/com/team/buddyya/student/service/StudentService.java
@@ -0,0 +1,33 @@
+package com.team.buddyya.student.service;
+
+import com.team.buddyya.student.domain.*;
+import com.team.buddyya.student.dto.request.OnBoardingRequest;
+import com.team.buddyya.student.exception.OnBoardingException;
+import com.team.buddyya.student.exception.OnBoardingExceptionType;
+import com.team.buddyya.student.repository.StudentRepository;
+import com.team.buddyya.student.repository.UniversityRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+    private final UniversityRepository universityRepository;
+
+    public Student createStudent(OnBoardingRequest request) {
+        University university = universityRepository.findByUniversityName(request.university())
+                .orElseThrow(() -> new OnBoardingException(OnBoardingExceptionType.UNIVERSITY_NOT_FOUND));
+        Student student = Student.builder()
+                .name(request.name())
+                .major(request.major())
+                .country(request.country())
+                .korean(request.korean())
+                .role(Role.STUDENT)
+                .university(university)
+                .gender(request.gender())
+                .build();
+        return studentRepository.save(student);
+    }
+}
