f3240ef350e84b96bf06d36ff0c4937838d513b4
fnsl1026 <fnsl1026@naver.com>
2024-11-11 18:50:34 +0900

    feat: OnBoarding을 담당하는 서비스 구현 (#3)

---
+++ src/main/java/com/team/buddyya/student/service/OnBoardingService.java
@@ -0,0 +1,28 @@
+package com.team.buddyya.student.service;
+
+import com.team.buddyya.student.domain.*;
+import com.team.buddyya.student.dto.request.OnBoardingRequest;
+import com.team.buddyya.student.dto.response.OnBoardingResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class OnBoardingService {
+
+    private final StudentService studentService;
+    private final AvatarService avatarService;
+    private final StudentInterestService studentInterestService;
+    private final StudentLanguageService studentLanguageService;
+
+    @Transactional
+    public OnBoardingResponse onboard(OnBoardingRequest request) {
+        Student student = studentService.createStudent(request);
+        avatarService.createAvatar(request, student);
+        studentInterestService.createStudentInterests(request, student);
+        studentLanguageService.createStudentLanguages(request, student);
+        return OnBoardingResponse.from(student);
+    }
+}
