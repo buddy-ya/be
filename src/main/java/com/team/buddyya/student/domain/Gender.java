90b2339fa1358a25b1909148cbaaef1c2dcbcc48
fnsl1026 <fnsl1026@naver.com>
2024-11-12 01:31:48 +0900

    feat: gender enum 타입을 string 타입과 대응 (#3)

--- src/main/java/com/team/buddyya/student/domain/Gender.java
+++ src/main/java/com/team/buddyya/student/domain/Gender.java
@@ -1,5 +1,25 @@
 package com.team.buddyya.student.domain;
 
+import lombok.Getter;
+
+import java.util.Arrays;
+
+@Getter
 public enum Gender {
-    MALE, FEMALE, UNKNOWN
+    MALE("male"),
+    FEMALE("female"),
+    UNKNOWN("unknown");
+
+    private final String displayName;
+
+    Gender(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public static Gender fromValue(String displayName) {
+        return Arrays.stream(Gender.values())
+                .filter(gender -> gender.getDisplayName().equals(displayName))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Invalid gender value: " + displayName));
+    }
 }
