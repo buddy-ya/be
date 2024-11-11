77e96ba0bca4f884bb5695a93dd9a54477699085
fnsl1026 <fnsl1026@naver.com>
2024-11-11 18:31:51 +0900

    feat: Interest 엔티티 정의 (#3)

--- src/main/java/com/team/buddyya/student/domain/Interest.java
+++ src/main/java/com/team/buddyya/student/domain/Interest.java
@@ -1,4 +1,22 @@
 package com.team.buddyya.student.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Table(name = "interest")
+@Getter
+@NoArgsConstructor(access = PROTECTED)
 public class Interest {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(length = 255, nullable = false)
+    private String interestName;
 }
