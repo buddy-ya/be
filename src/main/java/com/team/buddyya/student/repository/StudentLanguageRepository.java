7ba7b07bea6b7e208fee07e7bc90ea689f874b81
fnsl1026 <fnsl1026@naver.com>
2024-11-11 18:44:37 +0900

    feat: StudentLanguageRepository 생성 (#3)

---
+++ src/main/java/com/team/buddyya/student/repository/StudentLanguageRepository.java
@@ -0,0 +1,6 @@
+package com.team.buddyya.student.repository;
+
+import com.team.buddyya.student.domain.StudentLanguage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentLanguageRepository extends JpaRepository<StudentLanguage, Long> {}
