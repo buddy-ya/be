f82ccfb87ac5935a74d6659314913db972e47d42
maark1106 <mark1106@naver.com>
2024-11-15 17:15:22 +0900

    feat: 토큰 재발급 예외 처리 추가 (#12)

--- src/main/java/com/team/buddyya/auth/exception/AuthExceptionType.java
+++ src/main/java/com/team/buddyya/auth/exception/AuthExceptionType.java
@@ -12,8 +12,8 @@ public enum AuthExceptionType implements BaseExceptionType {
     UNAUTHORIZED_USER(301, HttpStatus.UNAUTHORIZED, "인가되지 않은 사용자입니다."),
     EXPIRED_TOKEN(302, HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
     INVALID_MEMBER_ID(303, HttpStatus.BAD_REQUEST, "유효하지 않은 사용자 ID 타입입니다."),
-    EXPIRED_REFRESH_TOKEN(304, HttpStatus.UNAUTHORIZED, "만료된 리프레시 토큰입니다."),
-    REFRESH_TOKEN_NOT_FOUND(305, HttpStatus.NOT_FOUND, "리프레시 토큰을 찾을 수 없습니다.");
+    REFRESH_TOKEN_NOT_FOUND(305, HttpStatus.NOT_FOUND, "리프레시 토큰을 찾을 수 없습니다."),
+    INVALID_REFRESH_TOKEN(305, HttpStatus.UNAUTHORIZED, "유효하지 않은 리프레시 토큰입니다.");
 
     private final int errorCode;
     private final HttpStatus httpStatus;
