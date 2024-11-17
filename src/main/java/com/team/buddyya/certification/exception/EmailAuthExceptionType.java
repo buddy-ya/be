c78d5fbec7b9f61d25b248189b78b196f27af387
maark1106 <mark1106@naver.com>
2024-11-17 10:29:07 +0900

    feat: 이메일 인증에서 발생하는 예외 처리 (#1)

---
+++ src/main/java/com/team/buddyya/certification/exception/EmailAuthExceptionType.java
@@ -0,0 +1,34 @@
+package com.team.buddyya.certification.exception;
+
+import com.team.buddyya.common.exception.BaseExceptionType;
+import org.springframework.http.HttpStatus;
+
+public enum EmailAuthExceptionType implements BaseExceptionType {
+
+    EMAIL_AUTH_FAILED(101, HttpStatus.UNAUTHORIZED, "이메일 인증 중 오류 발생");
+
+    private final int errorCode;
+    private final HttpStatus httpStatus;
+    private final String errorMessage;
+
+    EmailAuthExceptionType(int errorCode, HttpStatus httpStatus, String errorMessage) {
+        this.errorCode = errorCode;
+        this.httpStatus = httpStatus;
+        this.errorMessage = errorMessage;
+    }
+
+    @Override
+    public int errorCode() {
+        return errorCode;
+    }
+
+    @Override
+    public HttpStatus httpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String errorMessage() {
+        return errorMessage;
+    }
+}
--- src/main/java/com/team/buddyya/certification/service/CertificationService.java
+++ src/main/java/com/team/buddyya/certification/service/CertificationService.java
@@ -1,8 +1,11 @@
 package com.team.buddyya.certification.service;
 
+import com.team.buddyya.auth.domain.CustomUserDetails;
 import com.team.buddyya.certification.dto.request.EmailCertificationRequest;
 import com.team.buddyya.certification.dto.request.EmailCodeRequest;
 import com.team.buddyya.certification.dto.response.EmailCertificationResponse;
+import com.team.buddyya.certification.exception.EmailAuthException;
+import com.team.buddyya.certification.exception.EmailAuthExceptionType;
 import com.univcert.api.UnivCert;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,12 +19,17 @@ public class CertificationService {
 
     private String API_KEY = "c86153bb-9519-45ea-a185-3f5a8c50b0eb";
 
-    public EmailCertificationResponse certificateEmail(EmailCertificationRequest emailRequest) throws IOException {
-        Map<String, Object> univCertResponse = UnivCert.certify(API_KEY, emailRequest.email(), emailRequest.univName(), true);
-        return new EmailCertificationResponse((Boolean) univCertResponse.get("success"));
+    public EmailCertificationResponse certificateEmail(EmailCertificationRequest emailRequest) {
+        try {
+            Map<String, Object> univCertResponse = UnivCert.certify(API_KEY, emailRequest.email(), emailRequest.univName(), true);
+            boolean isSuccess = (Boolean) univCertResponse.get("success");
+            return new EmailCertificationResponse(isSuccess);
+        } catch (IOException e) {
+            throw new EmailAuthException(EmailAuthExceptionType.EMAIL_AUTH_FAILED);
+        }
     }
 
-    public EmailCertificationResponse certificateEmailCode(EmailCodeRequest codeRequest) throws IOException {
+    public EmailCertificationResponse certificateEmailCode(CustomUserDetails userDetails, EmailCodeRequest codeRequest) throws IOException {
         Map<String, Object> univCertResponse = UnivCert.certifyCode(API_KEY, codeRequest.email(), codeRequest.univName(), codeRequest.code());
         return new EmailCertificationResponse((Boolean) univCertResponse.get("success"));
     }
