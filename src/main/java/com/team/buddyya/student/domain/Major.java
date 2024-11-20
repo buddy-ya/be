0b54049c3b1c4abb8f3ba56d83be44f8d47fdeb9
fnsl1026 <fnsl1026@naver.com>
2024-11-20 14:21:17 +0900

    feat: 학생이 1개이상 major를 선택할 수 있으므로 major를 별도의 테이블로 분리 (#3)

---
+++ src/main/java/com/team/buddyya/student/domain/Major.java
@@ -0,0 +1,27 @@
+package com.team.buddyya.student.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Table(name = "major")
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class Major {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(length = 255, nullable = false)
+    private String majorName;
+
+    @Builder
+    public Major(String majorName) {
+        this.majorName = majorName;
+    }
+}
---
+++ src/main/java/com/team/buddyya/student/repository/StudentMajorRepository.java
@@ -0,0 +1,9 @@
+package com.team.buddyya.student.repository;
+
+import com.team.buddyya.student.domain.StudentMajor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentMajorRepository extends JpaRepository<StudentMajor, Long> {
+}
