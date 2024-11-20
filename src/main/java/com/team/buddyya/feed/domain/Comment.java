aa79adb5c2f42f32ab704a19d31c274bdc2dc67d
mangsuyo <mangsuyo3610@gmail.com>
2024-11-21 02:34:53 +0900

    refactor: Comment 엔티티 생성 (#22)

--- src/main/java/com/team/buddyya/feed/domain/Comment.java
+++ src/main/java/com/team/buddyya/feed/domain/Comment.java
@@ -1,4 +1,46 @@
 package com.team.buddyya.feed.domain;
 
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+import com.team.buddyya.student.domain.Student;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "comment")
+@Getter
+@NoArgsConstructor(access = PROTECTED)
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "student_id", nullable = false)
+    private Student student;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "feed_id", nullable = false)
+    private Feed feed;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Builder
+    public Comment(Student student, Feed feed, String content) {
+        this.student = student;
+        this.feed = feed;
+        this.content = content;
+    }
 }
