55e95bb1c5765720564d6eefff257196e3183437
mangsuyo <mangsuyo3610@gmail.com>
2024-11-22 20:12:43 +0900

    refactor: 피드 업데이트를 Put을 Patch로 수정 (#22)

--- src/main/java/com/team/buddyya/feed/controller/FeedController.java
+++ src/main/java/com/team/buddyya/feed/controller/FeedController.java
@@ -13,9 +13,9 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,7 +48,7 @@ public class FeedController {
         return ResponseEntity.ok(response);
     }
 
-    @PutMapping("/{feedId}")
+    @PatchMapping("/{feedId}")
     public ResponseEntity<Void> updateFeed(
             @AuthenticationPrincipal CustomUserDetails userDetails,
             @PathVariable Long feedId,
