9f7dea735e64dff4dc17290f8288f35242aff9f2
mangsuyo <mangsuyo3610@gmail.com>
2024-11-22 20:16:22 +0900

    refactor: record 변수 하나일 때 개행하지 않도록 (#22)

--- src/main/java/com/team/buddyya/feed/dto/response/FeedCreateResponse.java
+++ src/main/java/com/team/buddyya/feed/dto/response/FeedCreateResponse.java
@@ -1,8 +1,6 @@
 package com.team.buddyya.feed.dto.response;
 
-public record FeedCreateResponse(
-        Long id
-) {
+public record FeedCreateResponse(Long id) {
 
     public static FeedCreateResponse from(Long id) {
         return new FeedCreateResponse(id);
