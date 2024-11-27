b8001029d3078ee0dd68e6400811564ea313940e
maark1106 <mark1106@naver.com>
2024-11-27 16:11:19 +0900

    refactor: Feed 부분 Formdata 수정

--- src/main/java/com/team/buddyya/feed/dto/request/feed/FeedCreateRequest.java
+++ src/main/java/com/team/buddyya/feed/dto/request/feed/FeedCreateRequest.java
@@ -1,12 +1,13 @@
 package com.team.buddyya.feed.dto.request.feed;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
 public record FeedCreateRequest(
         String title,
         String content,
-        String category
+        String category,
+        List<MultipartFile> images
 ) {
-
-    public static FeedCreateRequest from(String title, String content, String category) {
-        return new FeedCreateRequest(title, content, category);
-    }
 }
