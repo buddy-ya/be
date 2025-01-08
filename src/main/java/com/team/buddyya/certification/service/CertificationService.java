7d7e5f859f9cb530e51753ed106889c36220fefa
mangsuyo <mangsuyo3610@gmail.com>
2025-01-08 21:53:35 +0900

    feat: 학생 탈퇴하기, 학생 인증 초기화 (#52)

--- src/main/java/com/team/buddyya/certification/service/CertificationService.java
+++ src/main/java/com/team/buddyya/certification/service/CertificationService.java
@@ -1,5 +1,7 @@
 package com.team.buddyya.certification.service;
 
+import static com.team.buddyya.common.domain.S3DirectoryName.STUDENT_ID_CARD;
+
 import com.team.buddyya.auth.domain.StudentInfo;
 import com.team.buddyya.certification.domain.StudentIdCard;
 import com.team.buddyya.certification.dto.request.EmailCertificationRequest;
@@ -18,17 +20,14 @@ import com.team.buddyya.student.exception.StudentExceptionType;
 import com.team.buddyya.student.repository.StudentRepository;
 import com.team.buddyya.student.service.StudentService;
 import com.univcert.api.UnivCert;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.util.Map;
-import java.util.Optional;
-
-import static com.team.buddyya.common.domain.S3DirectoryName.STUDENT_ID_CARD;
 
 @Service
 @RequiredArgsConstructor
@@ -48,7 +47,8 @@ public class CertificationService {
                 .orElseThrow(() -> new StudentException(StudentExceptionType.STUDENT_NOT_FOUND));
         validateStatusAndEmail(emailRequest, student);
         try {
-            Map<String, Object> univCertResponse = UnivCert.certify(univcertApiKey, emailRequest.email(), emailRequest.univName(), true);
+            Map<String, Object> univCertResponse = UnivCert.certify(univcertApiKey, emailRequest.email(),
+                    emailRequest.univName(), true);
             boolean isSuccess = (Boolean) univCertResponse.get("success");
             return CertificationResponse.from(isSuccess);
         } catch (IOException e) {
@@ -67,7 +67,8 @@ public class CertificationService {
 
     public CertificationResponse certificateEmailCode(StudentInfo studentInfo, EmailCodeRequest codeRequest) {
         try {
-            Map<String, Object> univCertResponse = UnivCert.certifyCode(univcertApiKey, codeRequest.email(), codeRequest.univName(), codeRequest.code());
+            Map<String, Object> univCertResponse = UnivCert.certifyCode(univcertApiKey, codeRequest.email(),
+                    codeRequest.univName(), codeRequest.code());
             Boolean isSuccess = (Boolean) univCertResponse.get("success");
             if (isSuccess) {
                 Student student = studentRepository.findById(studentInfo.id())
@@ -86,7 +87,8 @@ public class CertificationService {
         student.updateEmail(codeRequest.email());
     }
 
-    public CertificationResponse uploadStudentIdCard(StudentInfo studentInfo, SendStudentIdCardRequest sendStudentIdCardRequest) {
+    public CertificationResponse uploadStudentIdCard(StudentInfo studentInfo,
+                                                     SendStudentIdCardRequest sendStudentIdCardRequest) {
         MultipartFile file = sendStudentIdCardRequest.image();
         Student student = studentRepository.findById(studentInfo.id())
                 .orElseThrow(() -> new StudentException(StudentExceptionType.STUDENT_NOT_FOUND));
@@ -124,5 +126,13 @@ public class CertificationService {
         StudentIdCard studentIdCard = studentIdCardRepository.findByStudent_Id(studentInfo.id())
                 .orElseThrow(() -> new CertificateException(CertificateExceptionType.STUDENT_ID_CARD_NOT_FOUND));
         return StudentIdCardResponse.from(studentIdCard.getImageUrl());
+    }
+
+    public void refreshStudentCertification(StudentInfo studentInfo) {
+        Student student = studentRepository.findById(studentInfo.id())
+                .orElseThrow(() -> new StudentException(StudentExceptionType.STUDENT_NOT_FOUND));
+        student.updateIsCertificated(false);
+        student.updateEmail(null);
+        studentIdCardRepository.deleteByStudent(student);
     }
 }
