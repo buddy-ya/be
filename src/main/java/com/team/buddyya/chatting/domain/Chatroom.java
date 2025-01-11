3d9da0fc24e3b971ddbdc8edd534e230db831c99
maark1106 <mark1106@naver.com>
2025-01-11 15:54:45 +0900

    feat: chatroom 도메인 생성 (#57)

--- src/main/java/com/team/buddyya/chatting/domain/Chatroom.java
+++ src/main/java/com/team/buddyya/chatting/domain/Chatroom.java
@@ -1,4 +1,31 @@
 package com.team.buddyya.chatting.domain;
 
-public class Chatroom {
+import com.team.buddyya.common.domain.BaseTime;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "chatroom")
+public class Chatroom extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "chatroom", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ChatroomStudent> chatroomStudents;
+
+    @OneToMany(mappedBy = "chatroom", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Chat> chats;
+
+    public Chatroom() {
+        this.chatroomStudents = new ArrayList<>();
+        this.chats = new ArrayList<>();
+    }
 }
