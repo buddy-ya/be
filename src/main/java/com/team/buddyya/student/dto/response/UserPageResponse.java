1b5fc943fce0ad75f34e5c09e6806566f0252369
fnsl1026 <fnsl1026@naver.com>
2025-01-17 13:01:55 +0900

    refactor: 마이페이지 조회와 상대 페이지의 조회의 응답 DTO에 필요한 정보 추가 (#68)

--- src/main/java/com/team/buddyya/student/dto/response/UserPageResponse.java
+++ src/main/java/com/team/buddyya/student/dto/response/UserPageResponse.java
@@ -5,34 +5,24 @@ import com.team.buddyya.student.domain.Student;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public record MyPageResponse(
+public record UserPageResponse(
         String name,
         String country,
         String university,
         String gender,
         String profileImageUrl,
-
-        Boolean isCertificated,
-
-        Boolean isStudentIdCardRequested,
-
-        Boolean isKorean,
-
         List<String> majors,
         List<String> languages,
         List<String> interests
 ) {
 
-    public static MyPageResponse from(Student student, boolean isStudentIdCardRequested) {
-        return new MyPageResponse(
+    public static UserPageResponse from(Student student) {
+        return new UserPageResponse(
                 student.getName(),
                 student.getCountry(),
                 student.getUniversity().getUniversityName(),
                 student.getGender().getDisplayName(),
                 student.getProfileImage().getUrl(),
-                student.getIsCertificated(),
-                isStudentIdCardRequested,
-                student.getIsKorean(),
                 convertToStringList(student.getMajors()),
                 convertToStringList(student.getLanguages()),
                 convertToStringList(student.getInterests())
--- src/main/java/com/team/buddyya/student/service/MyPageService.java
+++ src/main/java/com/team/buddyya/student/service/MyPageService.java
@@ -1,12 +1,14 @@
 package com.team.buddyya.student.service;
 
 import com.team.buddyya.auth.domain.StudentInfo;
+import com.team.buddyya.certification.repository.StudentIdCardRepository;
 import com.team.buddyya.student.domain.Student;
 import com.team.buddyya.student.dto.request.MyPageUpdateInterestsRequest;
 import com.team.buddyya.student.dto.request.MyPageUpdateLanguagesRequest;
 import com.team.buddyya.student.dto.request.MyPageUpdateNameRequest;
 import com.team.buddyya.student.dto.response.MyPageResponse;
 import com.team.buddyya.student.dto.response.MyPageUpdateResponse;
+import com.team.buddyya.student.dto.response.UserPageResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,6 +24,7 @@ public class MyPageService {
     private final StudentLanguageService studentLanguageService;
     private final FindStudentService findStudentService;
     private final ProfileImageService profileImageService;
+    private final StudentIdCardRepository studentIdCardRepository;
 
     public MyPageUpdateResponse updateInterests(StudentInfo studentInfo, MyPageUpdateInterestsRequest request) {
         Student student = findStudentService.findByStudentId(studentInfo.id());
@@ -50,6 +53,14 @@ public class MyPageService {
     @Transactional(readOnly = true)
     public MyPageResponse getMyPage(StudentInfo studentInfo) {
         Student student = findStudentService.findByStudentId(studentInfo.id());
-        return MyPageResponse.from(student);
+        boolean isStudentIdCardRequested = studentIdCardRepository.findByStudent(student)
+                .isPresent();
+        return MyPageResponse.from(student,isStudentIdCardRequested);
+    }
+
+    @Transactional(readOnly = true)
+    public UserPageResponse getUserPage(long studentId) {
+        Student student = findStudentService.findByStudentId(studentId);
+        return UserPageResponse.from(student);
     }
 }
