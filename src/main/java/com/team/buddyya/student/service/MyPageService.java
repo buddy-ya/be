d8df8054b548c237649065a8afb9e64212558ae0
maark1106 <mark1106@naver.com>
2025-01-25 17:08:05 +0900

    feat: 개인 프로필 이미지 업데이트(기본 이미지로도 변경 가능) (#81)

--- src/main/java/com/team/buddyya/student/service/MyPageService.java
+++ src/main/java/com/team/buddyya/student/service/MyPageService.java
@@ -2,8 +2,10 @@ package com.team.buddyya.student.service;
 
 import com.team.buddyya.auth.domain.StudentInfo;
 import com.team.buddyya.certification.repository.StudentIdCardRepository;
+import com.team.buddyya.common.service.S3UploadService;
 import com.team.buddyya.student.domain.Student;
 import com.team.buddyya.student.dto.request.MyPageUpdateRequest;
+import com.team.buddyya.student.dto.request.UpdateProfileImageRequest;
 import com.team.buddyya.student.dto.response.MyPageUpdateResponse;
 import com.team.buddyya.student.dto.response.UserProfileResponse;
 import com.team.buddyya.student.exception.StudentException;
@@ -11,6 +13,9 @@ import com.team.buddyya.student.exception.StudentExceptionType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.team.buddyya.common.domain.S3DirectoryName.PROFILE_IMAGE;
+import static com.team.buddyya.student.domain.UserProfileDefaultImage.USER_PROFILE_DEFAULT_IMAGE;
 
 @Service
 @RequiredArgsConstructor
@@ -24,6 +29,7 @@ public class MyPageService {
     private final FindStudentService findStudentService;
     private final ProfileImageService profileImageService;
     private final StudentIdCardRepository studentIdCardRepository;
+    private final S3UploadService s3UploadService;
 
     public MyPageUpdateResponse updateUser(StudentInfo studentInfo, MyPageUpdateRequest request) {
         Student student = findStudentService.findByStudentId(studentInfo.id());
@@ -51,16 +57,21 @@ public class MyPageService {
         return MyPageUpdateResponse.from(UPDATE_SUCCESS_MESSAGE);
     }
 
-    public MyPageUpdateResponse updateProfileDefaultImage(StudentInfo studentInfo, String profileImageKey) {
+    public MyPageUpdateResponse updateUserProfileImage(StudentInfo studentInfo, boolean isDefault, UpdateProfileImageRequest request) {
         Student student = findStudentService.findByStudentId(studentInfo.id());
-        profileImageService.updateProfileDefaultImage(student, profileImageKey);
+        if (isDefault) {
+            profileImageService.updateUserProfileImage(student, USER_PROFILE_DEFAULT_IMAGE.getUrl());
+            return MyPageUpdateResponse.from(UPDATE_SUCCESS_MESSAGE);
+        }
+        String imageUrl = s3UploadService.uploadFile(PROFILE_IMAGE.getDirectoryName(), request.profileImage());
+        profileImageService.updateUserProfileImage(student, imageUrl);
         return MyPageUpdateResponse.from(UPDATE_SUCCESS_MESSAGE);
     }
 
     @Transactional(readOnly = true)
     public UserProfileResponse getUserProfile(StudentInfo studentInfo, Long userId) {
         Student student = findStudentService.findByStudentId(userId);
-        if(studentInfo.id()!=userId){
+        if (studentInfo.id() != userId) {
             return UserProfileResponse.from(student);
         }
         boolean isStudentIdCardRequested = studentIdCardRepository.findByStudent(student)
