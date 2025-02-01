e7ac9a4ccefd27dff17e42adf2578bea1f64db42
mangsuyo <mangsuyo3610@gmail.com>
2025-02-01 16:03:27 +0900

    feat: 프로필 이미지를 올린 유저인지 필드에 추가 (#55)

--- src/main/java/com/team/buddyya/feed/dto/response/comment/CommentResponse.java
+++ src/main/java/com/team/buddyya/feed/dto/response/comment/CommentResponse.java
@@ -1,6 +1,7 @@
 package com.team.buddyya.feed.dto.response.comment;
 
 import com.team.buddyya.feed.domain.Comment;
+import com.team.buddyya.student.domain.UserProfileDefaultImage;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -17,12 +18,14 @@ public record CommentResponse(
         boolean isDeleted,
         boolean isFeedOwner,
         boolean isCommentOwner,
+        boolean isProfileImageUpload,
         List<CommentResponse> replies
 ) {
 
     public static CommentResponse from(Comment comment, Long feedOwnerId, Long currentUserId) {
         boolean isFeedOwner = feedOwnerId.equals(comment.getStudent().getId());
         boolean isCommentOwner = currentUserId.equals(comment.getStudent().getId());
+        boolean isProfileImageUpload = UserProfileDefaultImage.isProfileImageUpload(comment.getStudent());
         List<CommentResponse> replies = comment.getChildren().stream()
                 .map(reply -> CommentResponse.from(reply, feedOwnerId, currentUserId))
                 .toList();
@@ -33,12 +36,13 @@ public record CommentResponse(
                 comment.getStudent().getName(),
                 comment.getStudent().getCountry(),
                 comment.getStudent().getUniversity().getUniversityName(),
-                comment.getStudent().getProfileImage().getUrl(),
+                comment.getStudent().getCharacterProfileImage(),
                 comment.getLikeCount(),
                 comment.getCreatedDate(),
                 comment.isDeleted(),
                 isFeedOwner,
                 isCommentOwner,
+                isProfileImageUpload,
                 replies
         );
     }
