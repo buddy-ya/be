edbca8ac510c5119e27fd206f14a496517918431
fnsl1026 <fnsl1026@naver.com>
2025-02-05 17:46:56 +0900

    feature: 학생 차단에 대한 엔티티 Block 생성 (#95)

---
+++ src/main/java/com/team/buddyya/student/domain/Block.java
@@ -0,0 +1,32 @@
+package com.team.buddyya.student.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Table(name = "block")
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class Block {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id", nullable = false)
+    private Student student;
+
+    @Column(name = "blocked_student_id", nullable = false)
+    private Long blockedStudentId;
+
+    @Builder
+    public Block(Student student, Long blockedStudentId) {
+        this.student = student;
+        this.blockedStudentId = blockedStudentId;
+    }
+}
