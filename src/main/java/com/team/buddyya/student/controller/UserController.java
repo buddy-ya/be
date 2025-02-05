cb5000e15f05fa7f62bc0cbe59d7afb6e32b4448
fnsl1026 <fnsl1026@naver.com>
2025-02-05 17:52:57 +0900

    feature: 학생 차단 controller 구현 (#95)

--- src/main/java/com/team/buddyya/student/controller/UserController.java
+++ src/main/java/com/team/buddyya/student/controller/UserController.java
@@ -3,8 +3,10 @@ package com.team.buddyya.student.controller;
 import com.team.buddyya.auth.domain.CustomUserDetails;
 import com.team.buddyya.feed.dto.response.feed.FeedListResponse;
 import com.team.buddyya.feed.service.FeedService;
+import com.team.buddyya.student.dto.request.BlockRequest;
 import com.team.buddyya.student.dto.request.MyPageUpdateRequest;
 import com.team.buddyya.student.dto.request.OnBoardingRequest;
+import com.team.buddyya.student.dto.response.BlockResponse;
 import com.team.buddyya.student.dto.response.UserResponse;
 import com.team.buddyya.student.dto.request.UpdateProfileImageRequest;
 import com.team.buddyya.student.service.OnBoardingService;
@@ -70,5 +72,12 @@ public class UserController {
     @DeleteMapping
     public void deleteStudent(@AuthenticationPrincipal CustomUserDetails userDetails) {
         studentService.deleteStudent(userDetails.getStudentInfo());
+    }
+
+    @PostMapping("/block")
+    public ResponseEntity<BlockResponse> blockStudent(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                                      @RequestBody BlockRequest request) {
+        BlockResponse response = studentService.blockStudent(userDetails.getStudentInfo().id(), request.blockedStudentId());
+        return ResponseEntity.ok(response);
     }
 }
