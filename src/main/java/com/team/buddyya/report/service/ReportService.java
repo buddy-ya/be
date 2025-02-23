da185843b64393852c464cb9540eb1744f0b8654
maark1106 <mark1106@naver.com>
2025-02-23 16:22:13 +0900

    refactor: feed, comment, chat 신고 방법 변경(#136)

--- src/main/java/com/team/buddyya/report/service/ReportService.java
+++ src/main/java/com/team/buddyya/report/service/ReportService.java
@@ -3,7 +3,9 @@ package com.team.buddyya.report.service;
 import com.team.buddyya.auth.domain.StudentInfo;
 import com.team.buddyya.chatting.service.ChatService;
 import com.team.buddyya.feed.service.CommentService;
+import com.team.buddyya.feed.service.FeedService;
 import com.team.buddyya.report.domain.Report;
+import com.team.buddyya.report.domain.ReportImage;
 import com.team.buddyya.report.domain.ReportType;
 import com.team.buddyya.report.dto.ReportRequest;
 import com.team.buddyya.report.repository.ReportRepository;
@@ -11,12 +13,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class ReportService {
 
     private final ReportRepository reportRepository;
+    private final FeedService feedService;
     private final CommentService commentService;
     private final ChatService chatService;
 
@@ -24,18 +30,46 @@ public class ReportService {
         validateReportedContent(request.type(), request.reportedId());
         Report report = Report.builder()
                 .type(request.type())
-                .reportedId(request.reportedId())
+                .reportedId(request.type() == ReportType.CHATROOM ? request.reportedId() : null)
                 .reportUserId(studentInfo.id())
                 .reportedUserId(request.reportedUserId())
-                .content(request.content())
+                .title(getTitle(request))
+                .content(getContent(request))
+                .reason(request.reason())
+                .images(getReportImages(request))
                 .build();
         reportRepository.save(report);
+    }
+
+    private String getTitle(ReportRequest request) {
+        if (request.type() == ReportType.FEED) {
+            return feedService.findFeedByFeedId(request.reportedId()).getTitle();
+        }
+        return null;
+    }
+
+    private String getContent(ReportRequest request) {
+        if (request.type() == ReportType.FEED) {
+            return feedService.findFeedByFeedId(request.reportedId()).getContent();
+        } else if (request.type() == ReportType.COMMENT) {
+            return commentService.findCommentByCommentId(request.reportedId()).getContent();
+        }
+        return null;
+    }
+
+    private List<ReportImage> getReportImages(ReportRequest request) {
+        if (request.type() == ReportType.FEED) {
+            return feedService.findFeedByFeedId(request.reportedId()).getImages().stream()
+                    .map(image -> new ReportImage(image.getUrl()))
+                    .collect(Collectors.toList());
+        }
+        return null;
     }
 
     private void validateReportedContent(ReportType type, Long id) {
         switch (type) {
             case FEED:
-                commentService.findFeedByFeedId(id);
+                feedService.findFeedByFeedId(id);
                 break;
             case COMMENT:
                 commentService.findCommentByCommentId(id);
