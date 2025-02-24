9115e800bfb5a02553cfa2a53ff7811f86ee211c
fnsl1026 <fnsl1026@naver.com>
2025-02-24 14:19:22 +0900

    feat: PointStatusRepository 생성 (#139)

---
+++ src/main/java/com/team/buddyya/student/repository/PointStatusRepository.java
@@ -0,0 +1,7 @@
+package com.team.buddyya.student.repository;
+
+import com.team.buddyya.student.domain.PointStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PointStatusRepository extends JpaRepository<PointStatus, Long> {
+}
