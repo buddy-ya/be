0c5ea0d10b1202a56028cea95dc34c56394372b7
maark1106 <mark1106@naver.com>
2025-02-26 11:13:31 +0900

    refactor: 개행 추가 (#136)

--- src/main/java/com/team/buddyya/report/repository/ReportImageRepository.java
+++ src/main/java/com/team/buddyya/report/repository/ReportImageRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ReportImageRepository extends JpaRepository<ReportImage, Long> {
+
     List<ReportImage> findByReportId(Long reportId);
 }
