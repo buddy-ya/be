41da876d6e97369e99ce0d655949493e008b1693
fnsl1026 <fnsl1026@naver.com>
2025-03-16 15:29:32 +0900

    refactor: MatchRequestRepository에 deleteByStudent 메서드와 findByStudentId 메서드 추가 (#145)

--- src/main/java/com/team/buddyya/match/repository/MatchRequestRepository.java
+++ src/main/java/com/team/buddyya/match/repository/MatchRequestRepository.java
@@ -1,11 +1,13 @@
 package com.team.buddyya.match.repository;
 
 import com.team.buddyya.match.domain.MatchRequest;
+import com.team.buddyya.student.domain.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MatchRequestRepository extends JpaRepository<MatchRequest, Long> {
@@ -17,6 +19,7 @@ public interface MatchRequestRepository extends JpaRepository<MatchRequest, Long
             """)
     List<MatchRequest> findAllMatches(boolean isKorean);
 
+    void deleteByStudent(Student student);
 
-    boolean existsByStudentId(Long studentId);
+    Optional<MatchRequest> findByStudentId(Long studentId);
 }
