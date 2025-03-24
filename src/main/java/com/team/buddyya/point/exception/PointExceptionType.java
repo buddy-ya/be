c6530f3818d95cb17b4980a5bbd531f1d3babc88
fnsl1026 <fnsl1026@naver.com>
2025-03-24 20:03:58 +0900

    refactor: PointExceptionType에 20xx 번대는 errorCode는 이미 사용되고 있으므로 90xx 번대로 변경 (#207)

--- src/main/java/com/team/buddyya/point/exception/PointExceptionType.java
+++ src/main/java/com/team/buddyya/point/exception/PointExceptionType.java
@@ -5,10 +5,10 @@ import org.springframework.http.HttpStatus;
 
 public enum PointExceptionType implements BaseExceptionType {
 
-    INVALID_POINT_TYPE(2100, HttpStatus.BAD_REQUEST, "Invalid point type."),
-    NEGATIVE_POINT(2101, HttpStatus.BAD_REQUEST, "Points cannot be less than zero."),
-    EXCEED_MAX_POINT(2102, HttpStatus.BAD_REQUEST, "Exceeding the maximum allowable points is not permitted."),
-    POINT_NOT_FOUND(2103, HttpStatus.NOT_FOUND, "The student's points do not exist.");
+    INVALID_POINT_TYPE(9000, HttpStatus.BAD_REQUEST, "Invalid point type."),
+    NEGATIVE_POINT(9001, HttpStatus.BAD_REQUEST, "Points cannot be less than zero."),
+    EXCEED_MAX_POINT(9002, HttpStatus.BAD_REQUEST, "Exceeding the maximum allowable points is not permitted."),
+    POINT_NOT_FOUND(9003, HttpStatus.NOT_FOUND, "The student's points do not exist.");
 
     private final int errorCode;
     private final HttpStatus httpStatus;
