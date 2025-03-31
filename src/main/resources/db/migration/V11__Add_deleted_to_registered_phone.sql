0034dc4125aaf93b7da5163ddfb2632922527854
fnsl1026 <fnsl1026@naver.com>
2025-03-31 11:32:43 +0900

    refactor: has_withdrawn 네이밍 관련 마이그레이션 스크립트 수정 (#219)

--- src/main/resources/db/migration/V11__Add_deleted_to_registered_phone.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE registered_phone_number
-    ADD COLUMN deleted BIT(1) NULL;
-
-UPDATE registered_phone_number
-SET deleted = 0
-WHERE deleted IS NULL;
-
-ALTER TABLE registered_phone_number
-    MODIFY COLUMN deleted BIT(1) NOT NULL;
