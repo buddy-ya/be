d4fd2e73130c99cd35aa093c93948eaa472c08ba
maark1106 <mark1106@naver.com>
2025-04-07 11:30:50 +0900

    refactor: 이미지 url 수정 (#281)

--- src/main/java/com/team/buddyya/student/domain/CharacterProfileImage.java
+++ src/main/java/com/team/buddyya/student/domain/CharacterProfileImage.java
@@ -8,14 +8,14 @@ import java.util.Arrays;
 
 @Getter
 public enum CharacterProfileImage {
-    CHARACTER_PROFILE_IMAGE_1("image1", "https://buddyya.s3.ap-northeast-2.amazonaws.com/default-profile-image/image_1.png"),
-    CHARACTER_PROFILE_IMAGE_2("image2", "https://buddyya.s3.ap-northeast-2.amazonaws.com/default-profile-image/image_2.png"),
-    CHARACTER_PROFILE_IMAGE_3("image3", "https://buddyya.s3.ap-northeast-2.amazonaws.com/default-profile-image/image_3.png"),
-    CHARACTER_PROFILE_IMAGE_4("image4", "https://buddyya.s3.ap-northeast-2.amazonaws.com/default-profile-image/image_4.png"),
-    CHARACTER_PROFILE_IMAGE_5("image5", "https://buddyya.s3.ap-northeast-2.amazonaws.com/default-profile-image/image_5.png"),
-    CHARACTER_PROFILE_IMAGE_6("image6", "https://buddyya.s3.ap-northeast-2.amazonaws.com/default-profile-image/image_6.png"),
-    CHARACTER_PROFILE_IMAGE_7("image7", "https://buddyya.s3.ap-northeast-2.amazonaws.com/default-profile-image/image_7.png"),
-    CHARACTER_PROFILE_IMAGE_8("image8", "https://buddyya.s3.ap-northeast-2.amazonaws.com/default-profile-image/image_8.png");
+    CHARACTER_PROFILE_IMAGE_1("image1", "https://buddyya.s3.ap-northeast-2.amazonaws.com/default-profile-image/image__1.png"),
+    CHARACTER_PROFILE_IMAGE_2("image2", "https://buddyya.s3.ap-northeast-2.amazonaws.com/default-profile-image/image__2.png"),
+    CHARACTER_PROFILE_IMAGE_3("image3", "https://buddyya.s3.ap-northeast-2.amazonaws.com/default-profile-image/image__3.png"),
+    CHARACTER_PROFILE_IMAGE_4("image4", "https://buddyya.s3.ap-northeast-2.amazonaws.com/default-profile-image/image__4.png"),
+    CHARACTER_PROFILE_IMAGE_5("image5", "https://buddyya.s3.ap-northeast-2.amazonaws.com/default-profile-image/image__5.png"),
+    CHARACTER_PROFILE_IMAGE_6("image6", "https://buddyya.s3.ap-northeast-2.amazonaws.com/default-profile-image/image__6.png"),
+    CHARACTER_PROFILE_IMAGE_7("image7", "https://buddyya.s3.ap-northeast-2.amazonaws.com/default-profile-image/image__7.png"),
+    CHARACTER_PROFILE_IMAGE_8("image8", "https://buddyya.s3.ap-northeast-2.amazonaws.com/default-profile-image/image__8.png");
 
     private final String key;
     private final String url;
