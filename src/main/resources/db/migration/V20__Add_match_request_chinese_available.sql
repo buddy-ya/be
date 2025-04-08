17ab5c5f8eb7f9361000b3305fd27ae302f40ffe
fnsl1026 <fnsl1026@naver.com>
2025-04-08 12:57:03 +0900

    chore: 매칭 프로필에서 외래키 추가 조건 마이그레이션 스크립트에서 제거 (#292)

--- src/main/resources/db/migration/V20__Add_match_request_chinese_available.sql
+++ src/main/resources/db/migration/V20__Add_match_request_chinese_available.sql
@@ -4,9 +4,6 @@ ALTER TABLE match_request
 ALTER TABLE match_request
     MODIFY chinese_available BIT (1) NOT NULL;
 
-ALTER TABLE matching_profile
-    ADD CONSTRAINT FK_MATCHING_PROFILE_ON_STUDENT FOREIGN KEY (student_id) REFERENCES student (id);
-
 ALTER TABLE match_request
 DROP
 COLUMN korean;
