391e406088e1d5bb1487635621a248d1874b9ca3
fnsl1026 <fnsl1026@naver.com>
2025-05-04 18:04:54 +0900

    refactor: 이메일 인증시 포인트 지급 로직 추가  (#327)

--- src/main/java/com/team/buddyya/certification/dto/response/CertificationResponse.java
+++ src/main/java/com/team/buddyya/certification/dto/response/CertificationResponse.java
@@ -1,8 +1,11 @@
 package com.team.buddyya.certification.dto.response;
 
-public record CertificationResponse(boolean success) {
+import com.team.buddyya.point.domain.Point;
+import com.team.buddyya.point.domain.PointType;
 
-    public static CertificationResponse from(boolean success) {
-        return new CertificationResponse(success);
+public record CertificationResponse(Integer point, Integer pointChange) {
+
+    public static CertificationResponse from(Point point, PointType pointType) {
+        return new CertificationResponse(point.getCurrentPoint(), pointType.getPointChange());
     }
 }
