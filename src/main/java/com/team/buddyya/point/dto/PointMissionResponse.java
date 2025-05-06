b6c4faf67cebaf7cf85c4891beca4562909ea96a
fnsl1026 <fnsl1026@naver.com>
2025-05-06 16:59:53 +0900

    refactor: 미션 상세 페이지에서 인증 여부는 단순 인증 여부가 아닌 인증 미션 이벤트 참여 여부이므로 hasCertificated로 수정  (#327)

--- src/main/java/com/team/buddyya/point/dto/PointMissionResponse.java
+++ src/main/java/com/team/buddyya/point/dto/PointMissionResponse.java
@@ -4,13 +4,13 @@ import com.team.buddyya.certification.domain.RegisteredPhone;
 import com.team.buddyya.student.domain.Student;
 
 public record PointMissionResponse(
-        boolean isCertificated,
+        boolean hasCertificated,
         boolean todayAttended,
         int totalMissionPoint
 ) {
 
-    public static PointMissionResponse from(Student student, RegisteredPhone registeredPhone, int totalMissionPoint) {
-        return new PointMissionResponse(student.getIsCertificated(), registeredPhone.isTodayAttended(), totalMissionPoint);
+    public static PointMissionResponse from(RegisteredPhone registeredPhone, int totalMissionPoint) {
+        return new PointMissionResponse(registeredPhone.getHasCertificated(), registeredPhone.isTodayAttended(), totalMissionPoint);
     }
 }
 
