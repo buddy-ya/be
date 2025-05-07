cf5c7fd20e3a2f7d258405afb8dda80c90119e4c
fnsl1026 <fnsl1026@naver.com>
2025-05-07 21:24:14 +0900

    refactor: 출석 체크시 참여 여부와 이벤트로 받은 총 포인트를 추가로 반환 (#333)

--- src/main/java/com/team/buddyya/mission/dto/PointMissionRewardResponse.java
+++ src/main/java/com/team/buddyya/mission/dto/PointMissionRewardResponse.java
@@ -1,11 +1,15 @@
 package com.team.buddyya.mission.dto;
 
+import com.team.buddyya.point.domain.Point;
+
 public record PointMissionRewardResponse(
-        Integer currentPoint,
-        int pointChange
+        Integer point,
+        int pointChange,
+        boolean todayAttended,
+        int totalMissionPoint
 ) {
 
-    public static PointMissionRewardResponse from(Integer currentPoint, int pointChange) {
-        return new PointMissionRewardResponse(currentPoint, pointChange);
+    public static PointMissionRewardResponse from(Point point, int pointChange, int totalMissionPoint) {
+        return new PointMissionRewardResponse(point.getCurrentPoint(), pointChange, true, totalMissionPoint);
     }
 }
