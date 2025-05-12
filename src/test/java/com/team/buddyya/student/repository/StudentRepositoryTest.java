7efac1b86c570e41cf6f769e3af0d0284e6eab61
fnsl1026 <fnsl1026@naver.com>
2025-05-12 19:06:04 +0900

    fix: StudentRepositoryTest는 db관련 환경 설정이 필요하기 때문에 @ActiveProfiles("test")를 명시 (#339)

--- src/test/java/com/team/buddyya/student/repository/StudentRepositoryTest.java
+++ src/test/java/com/team/buddyya/student/repository/StudentRepositoryTest.java
@@ -6,10 +6,12 @@ import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@ActiveProfiles("test")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
 class StudentRepositoryTest {
