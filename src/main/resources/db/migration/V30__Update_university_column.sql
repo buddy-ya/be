a43a647062fbfe112714f0ef8f9b8bf4c32f4400
mangsuyo <mangsuyo3610@gmail.com>
2025-05-13 12:50:33 +0900

    fix: 마이그레이션 오류 해결 (#347)

--- src/main/resources/db/migration/V30__Update_university_column.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE university
-    DROP COLUMN matchActive,
-    DROP COLUMN feedActive,
-    ADD COLUMN match_active BIT(1) NOT NULL DEFAULT 1,
-    ADD COLUMN feed_active  BIT(1) NOT NULL DEFAULT 0;
