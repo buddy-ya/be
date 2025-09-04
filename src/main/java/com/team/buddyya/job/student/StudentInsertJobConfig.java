593ea2802ef9b9ef3f3594d763804fbb91f1f803
mangsuyo <mangsuyo3610@gmail.com>
2025-09-04 13:23:48 +0900

    feat: local 프로파일에서만 배치가 동작한다 (#365)

--- src/main/java/com/team/buddyya/job/student/StudentInsertJobConfig.java
+++ src/main/java/com/team/buddyya/job/student/StudentInsertJobConfig.java
@@ -16,8 +16,10 @@ import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilde
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.PlatformTransactionManager;
 
+@Profile("local")
 @Configuration
 @RequiredArgsConstructor
 public class StudentInsertJobConfig {
